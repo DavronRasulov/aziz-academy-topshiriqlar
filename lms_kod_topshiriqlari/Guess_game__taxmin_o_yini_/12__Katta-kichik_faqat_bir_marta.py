84884b31ba43edc1d7fcf5ebec6c69865c61257f
DavronRasulov <umarovamatluba980@gmail.com>
2026-06-02 10:39:37 +0500

    Add: 12) Katta-kichik faqat bir marta [DavronRasulov]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/12__Katta-kichik_faqat_bir_marta.py
@@ -0,0 +1,20 @@
+yashirin_son = 8
+
+son = int(input())
+
+if son < yashirin_son:
+    print("Low")
+elif son > yashirin_son:
+    print("High")
+else:
+    print("Correct")
+    exit()
+    
+son = int(input())
+    
+if son < yashirin_son:
+    print("Low")
+elif son > yashirin_son:
+    print("High")
+else:
+    print("Correct")
