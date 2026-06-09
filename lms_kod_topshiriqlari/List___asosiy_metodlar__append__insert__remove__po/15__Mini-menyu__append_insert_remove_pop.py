c874d8c8e45b6f6f1c03892d678f21e350ac84f3
DavronRasulov <umarovamatluba980@gmail.com>
2026-06-09 08:42:28 +0500

    Add: 15) Mini-menyu: append/insert/remove/pop [DavronRasulov]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/15__Mini-menyu__append_insert_remove_pop.py
@@ -0,0 +1,19 @@
+lst = []
+
+while True:
+    cmd = input().split()
+    
+    if cmd[0] == "stop":
+        break
+    elif cmd[0] == "append":
+        lst.append(int(cmd[1]))
+    elif cmd[0] == "insert":
+        lst.insert(int(cmd[1]), int(cmd[2]))
+    elif cmd[0] == "remove":
+        x = int(cmd[1])
+        if x in lst:
+            lst.remove(x)
+    elif cmd[0] == "pop":
+        lst.pop(int(cmd[1]))
+        
+print(lst)
